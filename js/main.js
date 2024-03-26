4cf4e2dece4333220a87dfcaba2f90143e96c42d
Stanislav <datura1953@mail.ru>
2024-03-26 13:38:06 +0700

    slide 1 done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
